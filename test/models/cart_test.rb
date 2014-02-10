9b417f7fb01ae3354f78962a6851d4c456810487
Brian Eng <brian@brianeng.com>
2014-02-09 23:35:13 -0600

    fix

--- test/models/cart_test.rb
+++ test/models/cart_test.rb
@@ -2,8 +2,8 @@ require 'test_helper'
 
 class CartTest < ActiveSupport::TestCase
   setup do
-    mms = Product.create(name: 'M&Ms')
-    jersey = Product.create(name: 'Blackhawks Jersey')
+    @mms = Product.create(name: 'M&Ms')
+    @jersey = Product.create(name: 'Blackhawks Jersey')
     @products = Product.all
     @cart = Cart.first
   end
