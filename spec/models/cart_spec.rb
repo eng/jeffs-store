5fa434cd2a3ba016393239117cd6604d0046cb98
Will Barrett <will@tallgreentree.com>
2014-03-02 14:35:57 -0600

    Using the rspec-style matcher

--- spec/models/cart_spec.rb
+++ spec/models/cart_spec.rb
@@ -16,50 +16,50 @@ describe Cart do
   describe :add_product do
     it 'adds an item to cart' do
       @cart.add_product(@jersey)
-      assert_equal 1, @cart.items_count
+      @cart.items_count.should eq(1)
     end
 
     it 'updates quantity when the item is a duplicate' do
       @cart.add_product(@jersey)
       @cart.add_product(@jersey)
       first_item_in_cart = @cart.cart_items.first
-      assert_equal 2, first_item_in_cart.qty
+      first_item_in_cart.qty.should eq(2)
     end
   end
 
   describe :subtotal do
     it 'calculates the subtotal' do
       fill_cart
-      assert_equal 41.0, @cart.subtotal
+      @cart.subtotal.should eq(41.0)
     end
 
     it 'has a subtotal of 0 when empty' do
-      assert_equal 0.0, @cart.subtotal
+      @cart.subtotal.should eq(0.0)
     end
   end
 
   describe :tax do
     it "calculates the tax" do
       fill_cart
-      assert_equal 2.46, @cart.tax
+      @cart.tax.should eq(2.46)
     end
   end
 
   describe :shipping do
     it "calculates shipping" do
       fill_cart
-      assert_equal 5.0, @cart.shipping
+      @cart.shipping.should eq(5.0)
     end
 
     it "is 0 when the cart is empty" do
-      assert_equal 0.0, @cart.shipping
+      @cart.shipping.should eq(0)
     end
   end
 
   describe :total do
     it 'calculates the grand total' do
       fill_cart
-      assert_equal 48.46, @cart.total
+      @cart.total.should eq(48.46)
     end
   end
 
@@ -67,7 +67,7 @@ describe Cart do
     it 'removes an item from the cart' do
       fill_cart
       @cart.remove_product(@jersey)
-      assert_equal 1, @cart.items_count
+      @cart.items_count.should eq(1)
     end
   end
 
@@ -75,7 +75,7 @@ describe Cart do
     it 'empties the whole cart' do
       fill_cart
       @cart.empty
-      assert_equal 0, @cart.items_count
+      @cart.items_count.should eq(0)
     end
   end
 
