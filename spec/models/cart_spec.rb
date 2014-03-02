267a7108053780e02e3da0146222aadaa4e44286
Will Barrett <will@tallgreentree.com>
2014-03-02 16:30:26 -0600

    Added factories, using let in Brian's example

--- spec/models/cart_spec.rb
+++ spec/models/cart_spec.rb
@@ -1,28 +1,35 @@
 require 'spec_helper'
 
 describe Cart do
-  before :each do
-    @mms = Product.create(name: 'M&Ms', price: 1.0)
-    @jersey = Product.create(name: 'Blackhawks Jersey', price: 40.0)
-    @products = Product.all
-    @cart = Cart.first
-  end
+
+  # This is a literal translation, but it's not the best method -WHB
+  # before :each do
+  #   @mms = Product.create(name: 'M&Ms', price: 1.0)
+  #   @jersey = Product.create(name: 'Blackhawks Jersey', price: 40.0)
+  #   @products = Product.all
+  #   @cart = Cart.first
+  # end
+  
+  let (:mms) { FactoryGirl.create(:product, name: "M&Ms", price: 1.0) }
+  let (:jersey) { FactoryGirl.create(:product, name: "Blackhawks Jersey", price: 40.0) }
+  let (:products) { [mms, jersey] }
+  let (:cart) { FactoryGirl.create(:cart) }
 
   def fill_cart
-    @cart.add_product(@mms)
-    @cart.add_product(@jersey)
+    cart.add_product(mms)
+    cart.add_product(jersey)
   end
 
   describe :add_product do
     it 'adds an item to cart' do
-      @cart.add_product(@jersey)
-      @cart.items_count.should eq(1)
+      cart.add_product(jersey)
+      cart.items_count.should eq(1)
     end
 
     it 'updates quantity when the item is a duplicate' do
-      @cart.add_product(@jersey)
-      @cart.add_product(@jersey)
-      first_item_in_cart = @cart.cart_items.first
+      cart.add_product(jersey)
+      cart.add_product(jersey)
+      first_item_in_cart = cart.cart_items.first
       first_item_in_cart.qty.should eq(2)
     end
   end
@@ -30,52 +37,52 @@ describe Cart do
   describe :subtotal do
     it 'calculates the subtotal' do
       fill_cart
-      @cart.subtotal.should eq(41.0)
+      cart.subtotal.should eq(41.0)
     end
 
     it 'has a subtotal of 0 when empty' do
-      @cart.subtotal.should eq(0.0)
+      cart.subtotal.should eq(0.0)
     end
   end
 
   describe :tax do
     it "calculates the tax" do
       fill_cart
-      @cart.tax.should eq(2.46)
+      cart.tax.should eq(2.46)
     end
   end
 
   describe :shipping do
     it "calculates shipping" do
       fill_cart
-      @cart.shipping.should eq(5.0)
+      cart.shipping.should eq(5.0)
     end
 
     it "is 0 when the cart is empty" do
-      @cart.shipping.should eq(0)
+      cart.shipping.should eq(0)
     end
   end
 
   describe :total do
     it 'calculates the grand total' do
       fill_cart
-      @cart.total.should eq(48.46)
+      cart.total.should eq(48.46)
     end
   end
 
   describe :remove_product do 
     it 'removes an item from the cart' do
       fill_cart
-      @cart.remove_product(@jersey)
-      @cart.items_count.should eq(1)
+      cart.remove_product(jersey)
+      cart.items_count.should eq(1)
     end
   end
 
   describe :empty do
     it 'empties the whole cart' do
       fill_cart
-      @cart.empty
-      @cart.items_count.should eq(0)
+      cart.empty
+      cart.items_count.should eq(0)
     end
   end
 
